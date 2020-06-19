c485e19c76b33c7b6c56fe483df1e5b758f541cf
Michael Shi <michaelshi03@gmail.com>
2020-06-19 13:05:31 -0700

    Test ESLint

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-
+//test
 const schema = require('../database/schema.js');
 const Users = schema.User;
 const Photos = schema.Photo;
