d4f8fe36a1f6ebaa6908fb94576a747a811167d7
Kimo Silva <e.k.silva441@gmail.com>
2020-07-11 12:24:08 -0700

    update cass sql

--- cass.sql
+++ cass.sql
@@ -1,6 +1,15 @@
 DROP KEYSPACE IF EXISTS hotels;
 CREATE KEYSPACE gallery WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': '1'} AND durable_writes = 'true';
 
+CREATE TABLE hotels (
+  id UUID,
+  name text,
+  address text,
+  phone text,
+  website text,
+  cost smallint
+)
+
 CREATE TABLE photos (
   id UUID,
   hotelId INT NOT NULL,
@@ -9,4 +18,5 @@ CREATE TABLE photos (
   PRIMARY KEY (id, hotelId) 
 )
 
+COPY hotels (id, name, address, phone, website, cost) FROM '/path/to/bigfile.csv'
 COPY photos (id, hotelId, description, url) FROM '/path/to/bigfile.csv'
